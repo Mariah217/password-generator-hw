eca6694d2e15a7988d7ff1896052a8a3be9b02fa
Mariah <mariahjohnston217@outlook.com>
2022-05-17 21:44:51 -0400

    created for loop for remaining characters, created if statedment for random lower and uppercase, created chartypes.push

--- script.js
+++ script.js
@@ -7,11 +7,12 @@
 4. Display password*/
 
 //Variables for prompts with strings
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 //converts lowercase to uppercase
-var uppercase = lowerCase.toUpperCase()
-var number = "8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128"
-var special = "!@#$%&*"
+var uppercase = lowerCase.toUpperCase();
+var number = "0123456789";
+var special = "!@#$%&*";
+var charTypes = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -20,9 +21,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
+
 //user prompt for length of characters
 function generatePassword() {
   var finalPwd = "" //using as placeholder
@@ -30,56 +31,70 @@ function generatePassword() {
   console.log(size)
 
   //validations for checking if true/false
-  if (size >= 8 && size <= 128) {
+  if (size >= 0 && size <= 128) {
     var isLowercase = confirm("Would you like to include lowercase?")
-    console.log(isLowercase)
+    if(isLowercase===true){
+      charTypes.push("lowercase");
+    }
     var isUppercase = confirm("Would you like to include uppercase?")
-    console.log(isUppercase)
+  if(isUppercase===true){
+    charTypes.push("uppercase");
+  }
     var isNumber = confirm("Would you like to include numbers?")
-    console.log(isNumber)
+    if(isNumber===true){
+      charTypes.push("number");
+    }
     var isSpecial = confirm("Would you like to include special characters?")
-    console.log(isSpecial)
+    if(isSpecial===true){
+      charTypes.push("special");
+    }
 
-    //randomly selects the position of the lowercase letter
-    if (isLowercase === true) {
-      var positionsString = Math.floor(Math.random() * 26) //*26 changes it from decimal to whole # up to 26 for alphabet
-      console.log(positionsString)
-      finalPwd = finalPwd + lowerCase[positionsString] //assigns final password to get returned below
+    //for loop for remaining characters
+    for (i = 0; i < size; i++) {
+      var randomText = Math.floor(Math.random() * charTypes.length);
+    // loop for lowercase
+    if (randomText==="lowercase"){
+      var randomLower= Math.floor(Math.random() * 26);
     }
-    else {
+    // loop for uppercase
+    if (randomText==="uppercase"){
+      var randomUpper =
+    }
+      
+      //randomly selects the position of the lowercase letter
+      if (isLowercase === true) {
+        var positionsString = Math.floor(Math.random() * 26) //*26 changes it from decimal to whole # up to 26 for alphabet
+        console.log(positionsString)
+        finalPwd = finalPwd + lowerCase[positionsString] //assigns final password to get returned below
+      }
 
-    }
-    //randomly selects the position of the uppercase letter
-    if (isUppercase === true) {
-      var positionsString = Math.floor(Math.random() * 26)
-      console.log(positionsString)
-      finalPwd = finalPwd + uppercase[positionsString]
-    }
-    else {
+      //randomly selects the position of the uppercase letter
+      if (isUppercase === true) {
+        var positionsString = Math.floor(Math.random() * 26)
+        console.log(positionsString)
+        finalPwd = finalPwd + uppercase[positionsString]
+      }
 
-    }
-    //randomly selects the position of the number
-    if (isNumber === true) {
-      var positionsString = Math.floor(Math.random() * 120) //*120 changes it from a decimal to whole # up to 120 (range 8-128=120 diff numbers)
-      console.log(positionsString)
-      finalPwd = finalPwd + number[positionsString]
-    }
-    else {
+      //randomly selects the position of the number
+      if (isNumber === true) {
+        var positionsString = Math.floor(Math.random() * 10) //*10 changes it from a decimal to whole # up to 120 (range 8-128=120 diff numbers)
+        console.log(positionsString)
+        finalPwd = finalPwd + number[positionsString]
+      }
 
-    }
-    //randomly selects the position of the special character
-    if (isSpecial === true) {
-      var positionsString = Math.floor(Math.random() * 7) //*7 because there are 7 different special characters
-      console.log(positionsString)
-      finalPwd = finalPwd + special[positionsString]
+      //randomly selects the position of the special character
+      if (isSpecial === true) {
+        var positionsString = Math.floor(Math.random() * 7) //*7 because there are 7 different special characters
+        console.log(positionsString)
+        finalPwd = finalPwd + special[positionsString]
+      }
     }
   }
   else {
     alert("Invalid entry")
   }
   //displays the final password
-  return finalPwd
+  return finalPwd;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
