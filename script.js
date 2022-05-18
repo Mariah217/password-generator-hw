10868d1837a9d7c81a7ef2439fe45e5721f2e529
Mariah <mariahjohnston217@outlook.com>
2022-05-18 18:34:36 -0400

    added var before i line 54, deleted lines 77-103-not needed, updated lines 66, 70 and 74 from alpha to correct var

--- script.js
+++ script.js
@@ -27,82 +27,86 @@ function writePassword() {
 //user prompt for length of characters
 function generatePassword() {
   var finalPwd = "" //using as placeholder
-  var size = prompt("Please enter 8 to 128 characters")
-  console.log(size)
+  var sizeInput = prompt("Please enter 8 to 128 characters")
+  var size = parseInt (sizeInput)
+  console.log(typeof size)
 
   //validations for checking if true/false
   if (size >= 0 && size <= 128) {
     var isLowercase = confirm("Would you like to include lowercase?")
-    if(isLowercase===true){
+    if (isLowercase === true) {
       charTypes.push("lowercase");
     }
     var isUppercase = confirm("Would you like to include uppercase?")
-  if(isUppercase===true){
-    charTypes.push("uppercase");
-  }
+    if (isUppercase === true) {
+      charTypes.push("uppercase");
+    }
     var isNumber = confirm("Would you like to include numbers?")
-    if(isNumber===true){
+    if (isNumber === true) {
       charTypes.push("number");
     }
     var isSpecial = confirm("Would you like to include special characters?")
-    if(isSpecial===true){
+    if (isSpecial === true) {
       charTypes.push("special");
     }
 
     //for loop for remaining characters
-    for (i = 0; i < size; i++) {
-      var randomText = Math.floor(Math.random() * charTypes.length);
-    // loop for lowercase
-    if (randomText==="lowercase"){
-      var randomLower= Math.floor(Math.random() * 25);
-      finalPwd = finalPwd + alpha [randomLower];
-    }
+    for (var i = 0; i < size; i++) {
+      var randomText = charTypes[Math.floor(Math.random() * charTypes.length)];
 
-    if (randomText==="uppercase"){
-      var randomUpper= Math.floor(Math.random() * 25);
-      finalPwd = finalPwd + alpha [randomUpper];
-    }
-    if (randomText==="number"){
-      var randomNumber= Math.floor(Math.random() * 9);
-      finalPwd = finalPwd + alpha [randomNumber];
-    }
-    if (randomText==="special"){
-      var randomSpecial= Math.floor(Math.random() * 7);
-      finalPwd = finalPwd + alpha [randomSpecial];
-    }
+
+      // loop for lowercase
+      if (randomText === "lowercase") {
+        var randomLower = Math.floor(Math.random() * 26);
+        finalPwd = finalPwd + alpha[randomLower];
+      }
+ 
+      if (randomText === "uppercase") {
+        var randomUpper = Math.floor(Math.random() * 26);
+        finalPwd = finalPwd + uppercase[randomUpper];
+      }
+      if (randomText === "number") {
+        var randomNumber = Math.floor(Math.random() * 10);
+        finalPwd = finalPwd + number[randomNumber];
+      }
+      if (randomText === "special") {
+        var randomSpecial = Math.floor(Math.random() * 7);
+        finalPwd = finalPwd + special[randomSpecial];
+      }
 
       //randomly selects the position of the lowercase letter
-      if (isLowercase === true) {
-        var positionsString = Math.floor(Math.random() * 25) //*26 changes it from decimal to whole # up to 26 for alphabet
-        console.log(positionsString)
-        finalPwd = finalPwd + alpha[positionsString] //assigns final password to get returned below
-      }
+      // if (isLowercase === true) {
+      //   var positionsString = Math.floor(Math.random() * 26) //*26 changes it from decimal to whole # up to 26 for alphabet
+      //   console.log(positionsString)
+      //   finalPwd = finalPwd + alpha[positionsString] //assigns final password to get returned below
+      // }
 
-      //randomly selects the position of the uppercase letter
-      if (isUppercase === true) {
-        var positionsString = Math.floor(Math.random() * 25)
-        console.log(positionsString)
-        finalPwd = finalPwd + uppercase[positionsString]
-      }
+      // //randomly selects the position of the uppercase letter
+      // if (isUppercase === true) {
+      //   var positionsString = Math.floor(Math.random() * 26)
+      //   console.log(positionsString)
+      //   finalPwd = finalPwd + uppercase[positionsString]
+      // }
 
-      //randomly selects the position of the number
-      if (isNumber === true) {
-        var positionsString = Math.floor(Math.random() * 10) //*10 changes it from a decimal to whole # up to 120 (range 8-128=120 diff numbers)
-        console.log(positionsString)
-        finalPwd = finalPwd + number[positionsString]
-      }
+      // //randomly selects the position of the number
+      // if (isNumber === true) {
+      //   var positionsString = Math.floor(Math.random() * 10) //*10 changes it from a decimal to whole # up to 120 (range 8-128=120 diff numbers)
+      //   console.log(positionsString)
+      //   finalPwd = finalPwd + number[positionsString]
+      // }
 
-      //randomly selects the position of the special character
-      if (isSpecial === true) {
-        var positionsString = Math.floor(Math.random() * 7) //*7 because there are 7 different special characters
-        console.log(positionsString)
-        finalPwd = finalPwd + special[positionsString]
-      }
+      // //randomly selects the position of the special character
+      // if (isSpecial === true) {
+      //   var positionsString = Math.floor(Math.random() * 7) //*7 because there are 7 different special characters
+      //   console.log(positionsString)
+      //   finalPwd = finalPwd + special[positionsString]
+      // }
     }
   }
   else {
     alert("Invalid entry")
   }
+  console.log(finalPwd.length)
   //displays the final password
   return finalPwd;
 }
